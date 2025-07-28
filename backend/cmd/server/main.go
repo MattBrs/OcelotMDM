c47407334fdcf4b1b5db46ee820231a503a25558
MattBrs <matteobrusarosco@tutamail.com>
2025-07-28 08:54:54 +0200

    [mongoDB] add driver, connection and connection checks

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 // gin test structure
@@ -20,6 +27,33 @@ func ping(c *gin.Context) {
 func main() {
 	// TODO: initialize mongoDB connection, initialize services and add
 	// proper api calls
+	mongoUser := os.Getenv("MONGO_USERNAME")
+	mongoPassword := os.Getenv("MONGO_PASSWORD")
+	if mongoUser == "" || mongoPassword == "" {
+		panic(errors.New("mongo credentials are not set"))
+	}
+
+	mongoConnectionStr := fmt.Sprintf("mongodb+srv://%s:%s@ocelotmdm.oy5pj9q.mongodb.net/?retryWrites=true&w=majority&appName=OcelotMDM", mongoUser, mongoPassword)
+
+	serverApi := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoConnectionStr).SetServerAPIOptions(serverApi)
+
+	mongoClient, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = mongoClient.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err = mongoClient.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged mongoDb instance successfully")
 
 	router := gin.Default()
 	router.GET("/ping", ping) // just for testing if everything works, for now :)
